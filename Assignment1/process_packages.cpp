ad85498823de4a2d8fa7fa6b2925d8108e733e3c
Utku Eren <ue@uw.edu>
2017-11-14 10:31:16 -0800

    Add codes for Assignment1

---
+++ Assignment1/process_packages.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+#include <queue>
+#include <vector>
+
+struct Request {
+    Request(int arrival_time, int process_time):
+        arrival_time(arrival_time),
+        process_time(process_time)
+    {}
+
+    int arrival_time;
+    int process_time;
+};
+
+struct Response {
+    Response(bool dropped, int start_time):
+        dropped(dropped),
+        start_time(start_time)
+    {}
+
+    bool dropped;
+    int start_time;
+};
+
+class Buffer {
+public:
+    Buffer(int size):
+        size_(size),
+        finish_time_()
+    {}
+
+    Response Process(const Request &request) {
+        // write your code here
+		Response response(false,0);
+
+        if (finish_time_.empty())
+        	finish_time_.push(request.arrival_time + request.process_time);
+		else{
+			// check which processes are completed
+			while (!finish_time_.empty() && (finish_time_.front() <= request.arrival_time))
+				finish_time_.pop();	
+			
+			// decide which packets will be dropped
+			if (size_ <= finish_time_.size())
+				response.dropped = true;
+			else{
+				response.dropped = false;
+				if (finish_time_.empty())
+					response.start_time = request.arrival_time;
+				else
+					response.start_time = finish_time_.back();
+
+				// push the finish time of the new task to the queue
+				finish_time_.push(request.arrival_time + request.process_time);
+			}
+		}
+    	return response;
+    }
+
+private:
+    int size_;
+    std::queue <int> finish_time_;
+};
+
+std::vector <Request> ReadRequests() {
+    std::vector <Request> requests;
+    int count;
+    std::cin >> count;
+    for (int i = 0; i < count; ++i) {
+        int arrival_time, process_time;
+        std::cin >> arrival_time >> process_time;
+        requests.push_back(Request(arrival_time, process_time));
+    }
+    return requests;
+}
+
+std::vector <Response> ProcessRequests(const std::vector <Request> &requests, Buffer *buffer) {
+    std::vector <Response> responses;
+    for (int i = 0; i < requests.size(); ++i)
+        responses.push_back(buffer->Process(requests[i]));
+    return responses;
+}
+
+void PrintResponses(const std::vector <Response> &responses) {
+    for (int i = 0; i < responses.size(); ++i)
+        std::cout << (responses[i].dropped ? -1 : responses[i].start_time) << std::endl;
+}
+
+int main() {
+    int size;
+    std::cin >> size;
+    std::vector <Request> requests = ReadRequests();
+
+    Buffer buffer(size);
+    std::vector <Response> responses = ProcessRequests(requests, &buffer);
+
+    PrintResponses(responses);
+    return 0;
+}
